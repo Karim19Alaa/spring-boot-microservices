4a8a54f184b86559fc7bcdf5a495933b3f30f635
AbdelrahmanMosly <95633556+AbdelrahmanMosly@users.noreply.github.com>
2024-03-09 01:46:17 +0200

    Update RatingsResource.java

--- ratings-data-service/src/main/java/com/example/ratingsservice/resources/RatingsResource.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/resources/RatingsResource.java
@@ -2,6 +2,7 @@ package com.example.ratingsservice.resources;
 
 import com.example.ratingsservice.models.Rating;
 import com.example.ratingsservice.models.UserRating;
+import com.example.ratingsservice.repository.RatingRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,16 @@ import java.util.List;
 @RequestMapping("/ratings")
 public class RatingsResource {
 
+    private final RatingRepository ratingRepository;
+
+    public RatingsResource(RatingRepository ratingRepository) {
+        this.ratingRepository = ratingRepository;
+    }
+
+
     @RequestMapping("/{userId}")
     public UserRating getRatingsOfUser(@PathVariable String userId) {
-        List<Rating> ratings = Arrays.asList(
-                new Rating("550", 4)
-        );
-
+        List<Rating> ratings = ratingRepository.findByUserId(userId);
         return new UserRating(ratings);
     }
 }
