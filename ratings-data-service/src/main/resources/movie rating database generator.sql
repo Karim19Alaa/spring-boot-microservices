0c1095a3c85635b9c57e73faba3286b9ded35a97
AbdelrahmanMosly <95633556+AbdelrahmanMosly@users.noreply.github.com>
2024-03-09 03:05:37 +0200

    connecting mysql to project

--- ratings-data-service/src/main/resources/movie rating database generator.sql
+++ ratings-data-service/src/main/resources/movie rating database generator.sql
@@ -3,12 +3,10 @@ USE movieratingsdb;
 
 CREATE TABLE IF NOT EXISTS ratings (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    userId VARCHAR(50),
-    movieId VARCHAR(50),
+    user_id VARCHAR(50),
+    movie_id VARCHAR(50),
     rating INT
 );
-
-
 -- Create a stored procedure to insert 200 rows of sample data
 DELIMITER //
 CREATE PROCEDURE GenerateSampleData()
@@ -16,7 +14,7 @@ BEGIN
     DECLARE i INT DEFAULT 1;
 
     WHILE i <= 200 DO
-        INSERT INTO ratings (userId, movieId, rating)
+        INSERT INTO ratings (user_id, movie_id, rating)
         VALUES
         (CONCAT('user', FLOOR(1 + RAND() * 10)), CONCAT('movie', FLOOR(1 + RAND() * 20)), FLOOR(1 + RAND() * 5));
 
