7c94c96976aa8fc5b95549bd57920111fedeb5f2
Karim Alaa <karimalaa1912@gmail.com>
2024-03-12 02:01:41 +0200

    updated rating generation code

--- new-trending-movies-service/src/main/resources/movie rating database generator.sql
+++ new-trending-movies-service/src/main/resources/movie rating database generator.sql
@@ -7,18 +7,12 @@ CREATE TABLE IF NOT EXISTS ratings (
     movie_id VARCHAR(50),
     rating INT
 );
-CREATE TABLE IF NOT EXISTS movie (
-    movie_id VARCHAR(50) PRIMARY KEY,
-    name VARCHAR(255),
-    description TEXT
-);
+
 
 -----------------------------------------------------
 -- -- Use Python Script to generate sample data -- --
 -----------------------------------------------------
-
 Select * from ratings;
-
 
 ------------------------------------------------------------------------
 -- -- We dont need Movie table, it is handled by the api and mongo -- --
--- new-trending-movies-service/src/main/resources/ratings_generation.py
+++ new-trending-movies-service/src/main/resources/ratings_generation.py
@@ -17,6 +17,7 @@ import os
 # movie_ids = [os.path.basename(a['href']) for a in soup.find_all('a', attrs={'href': re.compile(r'/movie/'), 'class': 'image'})]
 
 movie_ids = ['1096197', '932420', '1011985', '1239251', '940551', '693134', '969492', '792307', '870404', '1072790', '787699', '984249', '438631', '848538', '866398', '1227816', '636706', '714567', '609681', '949429']
+user_ids = [f"user{i}" for i in range(1, 100)]
 print(len(movie_ids))
 
 conn = mysql.connector.connect(
@@ -26,10 +27,18 @@ conn = mysql.connector.connect(
     database="movieratingsdb"
 )
 
+seen = set()
 
-for i in range(200):
-    sql = f"INSERT INTO movieratingsdb.ratings (user_id, movie_id, rating) VALUES ('user{random.randint(1, 10)}', '{movie_ids[random.randint(0, len(movie_ids) - 1)]}', {random.randint(1, 5)});"
-    print(sql)
+for i in range(1000):
+    user = user_ids[random.randint(0, len(user_ids) - 1)]
+    movie = movie_ids[random.randint(0, len(movie_ids) - 1)]
+    while (user, movie) in seen:
+        user = user_ids[random.randint(0, len(user_ids) - 1)]
+        movie = movie_ids[random.randint(0, len(movie_ids) - 1)]
+
+    seen.add((user, movie))
+    sql = f"INSERT INTO movieratingsdb.ratings (user_id, movie_id, rating) VALUES ('{user}', '{movie}', {random.randint(2, 5)});"
+    # print(sql)
     conn.cursor().execute(sql)
 
 conn.commit()
