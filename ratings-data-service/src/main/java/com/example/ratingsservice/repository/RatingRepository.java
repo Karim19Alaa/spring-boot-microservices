2c4d5f8c167ee2de601c7fc72f8ff23f7d4cc912
Karim Alaa <karimalaa1912@gmail.com>
2024-03-12 02:24:13 +0200

    sql changes

--- ratings-data-service/src/main/java/com/example/ratingsservice/repository/RatingRepository.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/repository/RatingRepository.java
@@ -1,10 +1,13 @@
 package com.example.ratingsservice.repository;
 
 import com.example.ratingsservice.entity.RatingEntity;
+import com.example.ratingsservice.entity.RatingId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-public interface RatingRepository extends JpaRepository<RatingEntity, Long> {
+@Repository
+public interface RatingRepository extends JpaRepository<RatingEntity, RatingId> {
     List<RatingEntity> findByUserId(String userId);
 }
