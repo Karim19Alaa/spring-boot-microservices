a269ab674d0d46aed3b705478c04371677b20417
Karim Alaa <karimalaa1912@gmail.com>
2024-03-12 02:24:56 +0200

    gen script update

--- new-trending-movies-service/src/main/resources/ratings_generation.py
+++ new-trending-movies-service/src/main/resources/ratings_generation.py
@@ -33,7 +33,6 @@ cursor = conn.cursor()
 
 ratings_table_creation = """
 CREATE TABLE IF NOT EXISTS movieratingsdb.ratings (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id VARCHAR(50),
     movie_id VARCHAR(50),
     rating INT
